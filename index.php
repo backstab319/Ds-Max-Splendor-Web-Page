b16496eb429807d6eab630e7a1a59b3f862fec2c
backstab319 <dev.siddhartha.gupta@gmail.com>
2019-04-12 21:22:35 +0530

    Made basic homepage

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="utf-8">
+    <title>DS MAX</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="navbar-section home">
+        <navbar class="navbar navbar-expand-sm navbar-light text-light fixed-top">
+            <div class="container">
+                <a class="navbar-brand">DS MAX</a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#toggle"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="toggle">
+                    <div class="navbar-menu ml-auto">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link text-light" href="#about">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-light" href="index.php">Home</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </navbar>
+    </div>
+
+    <div class="background">
+        <div class="overlay d-flex justify-content-center align-items-center">
+            <div class="container col-lg-8 col-xl-8">
+                <h1 class="display-4 text-center text-justify text-light">Welcome to Ds max splendor apartments!</h1>
+                <p class="lead text-center text-justify text-light">Located in Chikkabanavara, Bangalore 560090</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="overlay-div d-flex justify-content-center align-items-center" id="about">
+        <div class="container col-lg-8 col-xl-8 mt-5">
+            <h1 class="display-4 text-center text-justify text-light">About DS Max Splendor</h1>
+            <p class="lead text-center text-justify text-light">The Ds max splendor apartments are one of the top apartment's in bangalore. They provide apartment's with 
+            the best facilities to enjoy, best community to be a part of, and a trusted association of resident's whome you can trust.</p>
+        </div>
+    </div>
+
+    <div class="overlay-div d-flex justify-content-center align-items-center">
+        <div class="container col-lg-8 col-xl-8">
+            <h1 class="display-4 text-center text-justify text-light">Flats in Ds Max Splendor</h1>
+            <p class="lead text-center text-justify text-light">DS MAX provides different types of flat's. The 3BHK flats have three bedroom's, one hall, two bathroom's, 
+            one kitchen and one balcony.</p>
+        </div>
+    </div>
+
+    <div class="overlay-div d-flex justify-content-center align-items-center">
+        <div class="container col-lg-8 col-xl-8">
+            <h1 class="display-4 text-center text-justify text-light">Association</h1>
+            <p class="lead text-center text-justify text-light">The association in ds max is robust. Any resident can bring out any issue's that they might be facing 
+            and the association will make sure that it is addressed. The association is for the people by the people. The association also take's 
+            various steps to make the resident's life by organising varius partie's and celebration's during festival's.</p>
+        </div>
+    </div>
+
+    <div class="overlay-div d-flex justify-content-center align-items-center">
+        <div class="container col-lg-8 col-xl-8">
+            <h1 class="display-4 text-center text-justify text-light">Facilities</h1>
+            <p class="lead text-center text-justify text-light">The Ds Max apartment's aim to provide combortable and healthy living space for it's fellow 
+            resident's. The apartment has its own park where childern can play and walking path around the facility for resident's to walk.</p>
+        </div>
+    </div>
+
+    <div class="overlay-div d-flex justify-content-center align-items-center">
+        <div class="container col-lg-8 col-xl-8">
+            <h1 class="display-4 text-center text-justify text-light">Gym</h1>
+            <p class="lead text-center text-justify text-light">A private gym has been provided for the fellow resident's of Ds max to sweat it out and keep themselves healthy and fit. 
+            Children below the age of 16 are prohibited for saftey reason's.</p>
+        </div>
+    </div>
+
+    <div class="overlay-div d-flex justify-content-center align-items-center">
+        <div class="container col-lg-8 col-xl-8">
+            <h1 class="display-4 text-center text-justify text-light">Swimming pool</h1>
+            <p class="lead text-center text-justify text-light">A private swimming pool has been provided for all the resident's to have fun with friend's and familie's.</p>
+        </div>
+    </div>
+
+    <div class="overlay-div d-flex justify-content-center align-items-center">
+        <div class="container col-lg-8 col-xl-8">
+            <h1 class="display-4 text-center text-justify text-light">Security</h1>
+            <p class="lead text-center text-justify text-light">The whole premises is watched by security guard's and cctv camera's have been installed all around the premises.</p>
+        </div>
+    </div>
+
+    <div class="overlay-div d-flex justify-content-center align-items-center">
+        <div class="container col-lg-8 col-xl-8">
+            <h1 class="display-4 text-center text-justify text-light">Elevator</h1>
+            <p class="lead text-center text-justify text-light">All the four block's have been provided with elevator's to enable comfortable living.</p>
+        </div>
+    </div>
+    
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
